d28fc7efa82a9ad0bccc88a15461a9afb1eb981a
Jared Wasinger <j-wasinger@hotmail.com>
2018-01-15 14:12:31 +0000

    add WASM backticks

--- swap.py
+++ swap.py
@@ -7,12 +7,20 @@ def endian_swap(val):
     end = len(val)-(i+2)
 
     if i >= end:
-      return "".join(val)
+      break
 
     tmp = val[i:i+2]
     val[i:i+2] = val[end:end+2]
     val[end:end+2] = tmp
 
+  res = "" 
+  for i in range(0, len(val), 2):
+    res += "\\"+"".join(val[i:i+2])
+
+  return res
+
+print(endian_swap("deadbeef00000000000000000000000000000000"))
+
 if __name__ == "__main__":
-  assert endian_swap("1234") == "3412"
+  assert endian_swap("1234") == "\\34\\12"
   assert endian_swap("123") == None
