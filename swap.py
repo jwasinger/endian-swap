ddd0172dd99d6c2ef57caa0681589b6470c472b9
Jared Wasinger <j-wasinger@hotmail.com>
2018-02-28 16:56:40 +0000

    add function description, make big-endian to little endian optional

--- swap.py
+++ swap.py
@@ -1,19 +1,21 @@
 import sys
 
-def endian_swap(val):
+# convert a big-endian bytecode value to little endian, placing '\' between bytes to appease the wasm gods
+def endian_swap_wasm(val, littleEndian=True):
   val = list(val)
   if len(val) % 2 != 0:
     return None
 
-  for i in range(0, len(val), 2):
-    end = len(val)-(i+2)
+  if littleEndian:
+    for i in range(0, len(val), 2):
+      end = len(val)-(i+2)
 
-    if i >= end:
-      break
+      if i >= end:
+        break
 
-    tmp = val[i:i+2]
-    val[i:i+2] = val[end:end+2]
-    val[end:end+2] = tmp
+      tmp = val[i:i+2]
+      val[i:i+2] = val[end:end+2]
+      val[end:end+2] = tmp
 
   res = "" 
   for i in range(0, len(val), 2):
@@ -21,4 +23,4 @@ def endian_swap(val):
 
   return res
 
-print(endian_swap(sys.argv[1]))
+print(endian_swap_wasm(sys.argv[1], len(sys.argv) > 2 and sys.argv[2] == 'littlendian'))
